fe1f7258ad045d686f450890cb093161b4a5ee29
GulyasGergely0 <73882380+GulyasGergely0@users.noreply.github.com>
2022-01-18 15:05:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Szavak.
+A program kérjen be két szót a felhasználótól, majd írja ki, hogy melyik a hosszabb! 
+A program üzeneteinek megfogalmazásában kövesse az alábbi mintát:
+-----
+Adj meg egy szót! kelkáposzta
+Adj meg egy másik szót! kisegér
+A hosszabb szó: kelkáposzta
+---- 
+Adj meg egy szót! kelkáposzta
+Adj meg egy másik szót! káposztafej
+A két szó egyforma hosszú.
+----- 
+Adj meg egy szót! árvíztűrő
+Adj meg egy másik szót! tükörfúrógép
+A hosszabb szó: tükörfúrógép
+'''
+
+szo=input("Adj meg egy szót! ")
+szo2=input("Adj meg egy másik szót! ")
+if len(szo) > len(szo2):
+  print("A hosszabb szó: ", szo)
+elif len(szo2) > len(szo):
+  print("A hosszabb szó: ", szo2)
